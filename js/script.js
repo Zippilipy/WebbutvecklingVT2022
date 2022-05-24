d6ab53ec3e838b5b75e85dd58267bc0ed48c022e
simone <04sien@skola.svedala.se>
2022-05-24 16:13:43 +0200

    Final site

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
+function hamburgerFunction() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
         x.style.display = "none";
     } else {
         x.style.display = "block";
+    }
+}
+function sidebarFunction() {
+    var x = document.getElementById("sideBar");
+    if (x.style.display === "block"){
+        x.style.display = "none";
+        document.getElementsByClassName("screenShadow")[0].remove()
+    } else {
+        x.style.display = "block";
+        document.getElementsByTagName("body")[0].innerHTML = "<div onclick=\"sidebarFunction()\" class=\"screenShadow\"></div>" + document.getElementsByTagName("body")[0].innerHTML
     }
 }
